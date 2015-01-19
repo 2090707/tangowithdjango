d86fd3e4906e19597d4d0dd7b7c08be5ab8600cd
Aleksandar Ilov <2090707I@student.gla.ac.uk>
2015-01-19 21:35:13 +0000

    Chapter 5 - completed

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there world! <br/> <a href='/rango/about'>About</a>")
+    context_dict = {'boldmessage': "I am bold from the context"}
+    return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("<h2>This tutorial has been put together by Aleksandar Ilov, 2090707</h2> <br/> Rango says here is the about page. <br/> <a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html', {})
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -28,6 +30,10 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
+
+TEMPLATE_DIRS = [
+    TEMPLATE_PATH,
+]
 
 INSTALLED_APPS = (
     'django.contrib.admin',
@@ -82,3 +88,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    STATIC_PATH,
+    MEDIA_ROOT,
+)
+
+# Media Files
+
+MEDIA_URL = '/media/'
