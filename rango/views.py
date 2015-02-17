5fbeb5b3383281c7bba5a43291ea9c4505c99bee
Aleksandar Ilov <2090707I@student.gla.ac.uk>
2015-02-17 19:37:27 +0000

    Chapter 11 - completed

--- rango/views.py
+++ rango/views.py
@@ -4,16 +4,47 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from datetime import datetime
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list}
     context_dict['pages_by_views'] = Page.objects.order_by('-views')[0:5]
 
-    return render(request, 'rango/index.html', context_dict)
+    # get number of visits to the site
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+    reset_last_visit_time = False
+    context_dict['visits'] = visits
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        # if it's been more than a day since last visit
+        if(datetime.now() - last_visit_time).days > 0:
+            visits = visits + 1
+            reset_last_visit_time = True
+    else:
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+        context_dict['visits'] = visits
+
+    response = render(request, 'rango/index.html', context_dict)
+
+    return response
 
 def about(request):
-    return render(request, 'rango/about.html', {})
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 0
+
+    return render(request, 'rango/about.html', {'visits' : count})
 
 def category(request, category_name_slug):
     context_dict = {}
